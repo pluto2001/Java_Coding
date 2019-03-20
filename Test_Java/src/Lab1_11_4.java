1baa0b28cd97d147f293d4aa8fc2f28e3751e356
pluto2001 <pitsamaplurao@gmail.com>
2019-03-21 02:13:01 +0700

    Update Lab1_11_4

--- Test_Java/src/Lab1_11_4.java
+++ Test_Java/src/Lab1_11_4.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
 
 public class Lab1_11_4 {
+	
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		float cost = 0;
+		float profit = 0;
+		System.out.print("Cost =");
+		cost = Math.round(Float.parseFloat(sc.nextLine()));
+		System.out.print("% Profit =");
+		profit = Math.round(Float.parseFloat(sc.nextLine()));
+		System.out.print("Sale Price =");
+		System.out.println(((cost*profit)/100)+cost);
 
 	}
 
